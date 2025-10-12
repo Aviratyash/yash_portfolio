0535c181b3cc24f1ab7eeb1ba2fbe6e7bf09cf45
Yash Rajeshwar Ingole <101279464+Aviratyash@users.noreply.github.com>
2025-10-12 21:34:34 +0530

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,9 +1,10 @@
 export const siteMetadata = {
-name: 'Yash Rajeshwar Ingole',
-title: "M.Sc Cybersecurity | Threat Hunter & Digital Forensics",
-email: 'yashingole2003@gmail.com',
-phone: '9112358859',
-linkedin: 'https://linkedin.com/in/yash-ingole-468108263',
-github: '',
-resume: '/resume.pdf'
+  name: 'Yash Rajeshwar Ingole', // [cite: 1]
+  title: "M.Sc Cybersecurity | Security Operations & Threat Hunting", // Based on [cite: 2, 38, 49]
+  email: 'yashingole2003@gmail.com', // [cite: 3]
+  phone: '9112358859', // [cite: 6]
+  linkedin: 'https://linkedin.com/in/yash-ingole-468108263', // [cite: 7]
+  portfolio: 'https://yash-ingole.carrd.co', // 
+  github: '', // This remains empty as it's not in the resume
+  resume: '/resume.pdf'
 }
