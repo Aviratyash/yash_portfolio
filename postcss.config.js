e0c6fc650a48f297d8ab32ac7e69684509944e23
Yash Rajeshwar Ingole <101279464+Aviratyash@users.noreply.github.com>
2025-10-12 17:59:48 +0530

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+plugins: {
+tailwindcss: {},
+autoprefixer: {},
+},
+}
+
+
+---
+
+
+--- FILE: app/config.js
+export const siteMetadata = {
+name: 'Yash Rajeshwar Ingole',
+title: "M.Sc Cybersecurity | Threat Hunter & Digital Forensics",
+email: 'yashingole2003@gmail.com',
+phone: '9112358859',
+linkedin: 'https://linkedin.com/in/yash-ingole-468108263',
+github: '',
+resume: '/resume.pdf'
+}
