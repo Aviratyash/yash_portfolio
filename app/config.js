74962058fcdee2d6e16a6d4cb1a38bb44f35db32
Yash Rajeshwar Ingole <101279464+Aviratyash@users.noreply.github.com>
2025-10-12 18:01:09 +0530

    Create config.js

---
+++ app/config.js
@@ -0,0 +1,9 @@
+export const siteMetadata = {
+name: 'Yash Rajeshwar Ingole',
+title: "M.Sc Cybersecurity | Threat Hunter & Digital Forensics",
+email: 'yashingole2003@gmail.com',
+phone: '9112358859',
+linkedin: 'https://linkedin.com/in/yash-ingole-468108263',
+github: '',
+resume: '/resume.pdf'
+}
