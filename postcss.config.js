ca120671fff5acbbbfb1cc6c2442d756af2d495a
Yash Rajeshwar Ingole <101279464+Aviratyash@users.noreply.github.com>
2025-10-12 18:00:31 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -5,17 +5,3 @@ autoprefixer: {},
 },
 }
 
-
----
-
-
---- FILE: app/config.js
-export const siteMetadata = {
-name: 'Yash Rajeshwar Ingole',
-title: "M.Sc Cybersecurity | Threat Hunter & Digital Forensics",
-email: 'yashingole2003@gmail.com',
-phone: '9112358859',
-linkedin: 'https://linkedin.com/in/yash-ingole-468108263',
-github: '',
-resume: '/resume.pdf'
-}
